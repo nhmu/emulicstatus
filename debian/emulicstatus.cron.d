2f75a0591fdb94767fc9bca4d925c8b3a8ec09e5
Will Aoki <waoki@umnh.utah.edu>
2017-11-30 16:45:21 -0700

    corrected example, made 0.2 release

--- debian/emulicstatus.cron.d
+++ debian/emulicstatus.cron.d
@@ -11,6 +11,6 @@ EMUPATH=/opt/emu/umnh
 #
 # and uncomment this example:
 #
-# */15 * * * * www-data [ -x /usr/bin/licstatus] && [ -f /var/www/spool/emulicstatus/emu.rrd] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode-graph --graph-to /var/www/html/licstatus.png
+# */15 * * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus.png
 
 # vim: set syntax=crontab:
