6004af00bf8081ef4052a7ec713e85863f461714
Will Aoki <waoki@umnh.utah.edu>
2020-01-23 14:40:42 -0700

    Improved example

--- debian/emulicstatus.cron.d
+++ debian/emulicstatus.cron.d
@@ -16,6 +16,7 @@ EMUPATH=/opt/emu/umnh
 # The following are for use with /usr/share/doc/emulicstatus/examples/licstatus.html
 #
 # */30 * * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-7.png --graph-days 7
+# */30 * * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-14.png --graph-days 14
 # 2 */2 * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-31.png --graph-days 31
 # 31 8 * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-365.png --graph-days 365
 
