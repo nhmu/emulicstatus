f2b1fc3842d87a4165b25d8345ed82df64059e13
Will Aoki <waoki@umnh.utah.edu>
2020-01-23 13:45:57 -0700

    Examples for how to use the new support for multiple days

--- debian/emulicstatus.cron.d
+++ debian/emulicstatus.cron.d
@@ -13,4 +13,10 @@ EMUPATH=/opt/emu/umnh
 #
 # */15 * * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus.png
 
+# The following are for use with /usr/share/doc/emulicstatus/examples/licstatus.html
+#
+# */30 * * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-7.png --graph-days 7
+# 2 */2 * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-31.png --graph-days 31
+# 31 8 * * * www-data [ -x /usr/bin/licstatus ] && [ -f /var/spool/emulicstatus/emu.rrd ] && [ -d /var/www/html ] && /usr/bin/licstatus --rrdfile /var/spool/emulicstatus/emu.rrd --mode=graph --graph-to /var/www/html/licstatus-365.png --graph-days 365
+
 # vim: set syntax=crontab:
